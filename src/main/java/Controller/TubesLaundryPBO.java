61de0f9658ff4cbc2169539209e21b01de0bdc58
flxnzz <itszyro.47@gmail.com>
2023-03-20 18:19:13 +0700

    progress 1.0 : register user's view

---
+++ src/main/java/Controller/TubesLaundryPBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Controller;
+
+/**
+ *
+ * @author Haikal
+ */
+public class TubesLaundryPBO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
